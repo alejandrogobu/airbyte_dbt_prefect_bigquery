316aa87198f03182e9b90b7dcb1441b7fbdaa827
Alejandro González Bueno <alejandro.gonzalez.bu@gmail.com>
2024-01-27 18:36:53 +0100

    new file:   README.md new file:   dbt_project/.gitignore new file:   dbt_project/README.md new file:   dbt_project/analyses/.gitkeep new file:   dbt_project/dbt_project.yml new file:   dbt_project/macros/.gitkeep new file:   dbt_project/models/marts/product_popularity.sql new file:   dbt_project/models/marts/purchase_patterns.sql new file:   dbt_project/models/marts/user_demographics.sql new file:   dbt_project/models/sources/faker_sources.yml asdjasjd

---
+++ infra/airbyte/variables.tf
@@ -0,0 +1,15 @@
+variable "workspace_id" {
+    type = string
+    default = "89473199-48d0-4c96-8737-f0d7ee11dd9c"
+}
+
+variable "project_id" {
+    type = string
+    default = "civ-dev-dataengineering"
+}
+
+variable "credentials_json_path" {
+    type = string
+    default = "/Users/alejandrogonzalezbueno/Projects/bigquery/civ-dev-dataengineering-8c6359b2eb7f.json"
+
+}
